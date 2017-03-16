cf06d02abd73b0df8c6f74a0379e549f15bdbfe2
mariogiampieri <mariogiampieri@gmail.com>
2017-03-15 22:59:57 -0400

    initial commit
    Initial commit- includes ped and face detection sample scripts, sample
    image set, etc.

---
+++ faces.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+from imutils import paths
+import imutils
+
+face_cascade = cv2.CascadeClassifier('ocv2/opencv/data/haarcascades/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('ocv2/opencv/data/haarcascades/haarcascade_eye.xml')
+
+image_dir = "test_set/"
+test_img = "test_set/G0061813.jpg"
+
+for imagePath in paths.list_images(image_dir):
+
+    img = cv2.imread(imagePath)
+    img = imutils.resize(img, width=min(600, img.shape[1]))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ process_opencv.py
@@ -0,0 +1,63 @@
+# ped image detection drawn from http://www.pyimagesearch.com/2015/11/09/pedestrian-detection-opencv/
+
+import os
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import argparse
+import imutils
+import cv2
+import datetime
+
+image_dir = "test_set/"
+
+# construct the argument parse and parse the arguments - useful for invoking from shell
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--images", required=True, help="path to images directory")
+# args = vars(ap.parse_args())
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# loop over the image paths
+for imagePath in paths.list_images(image_dir):
+    # load the image and resize it to (1) reduce detection time
+    # and (2) improve detection accuracy
+    print imagePath
+    image = cv2.imread(imagePath)
+    print image
+    image = imutils.resize(image, width=min(800, image.shape[1]))
+    orig = image.copy()
+
+    # detect people in the image
+    start = datetime.datetime.now() # sets up a timer
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+                                            padding=(8, 8), scale=1.5)
+
+    print("[INFO] detection took: {}s".format(
+        (datetime.datetime.now() - start).total_seconds()))
+
+    # draw the original bounding boxes
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    # apply non-maxima suppression to the bounding boxes using a
+    # fairly large overlap threshold to try to maintain overlapping
+    # boxes that are still people
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    print len(rects)
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+    # show some information on the number of bounding boxes
+    filename = imagePath[imagePath.rfind("/") + 1:]
+    print("[INFO] {}: {} original boxes, {} after suppression".format(
+        filename, len(rects), len(pick)))
+
+    # show the output images
+    cv2.imshow("Before NMS", orig)
+    cv2.imshow("After NMS", image)
+    cv2.waitKey(0)
